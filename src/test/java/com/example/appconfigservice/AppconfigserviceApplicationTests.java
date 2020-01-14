521d3542fa66b29f6121e328244ee9847c45f000
AyoubDali <ayoub.dali@ensi-uma.tn>
2020-01-14 17:12:31 +0100

    Initial commit

---
+++ src/test/java/com/example/appconfigservice/AppconfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppconfigserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
